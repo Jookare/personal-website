94d8257c54fa549211f4070ae0f3efd36639bb5f
Jookare <134264039+Jookare@users.noreply.github.com>
2024-07-10 00:28:07 +0300

    Add small changes

--- app/components/navbar.js
+++ app/components/navbar.js
@@ -5,11 +5,23 @@ import Image from "next/image"
 export default function Navbar(props) {
 
     const { scrollToTop } = props;
+
+    const buttonPress = () => {
+        if (window.scrollY > 25) {
+            scrollToTop();
+        }
+        else {
+            window.location.reload();
+
+        }
+    }
+
     return (
-        <div className="flex fixed top-0 left-0 p-2">
-            <button className="btn transition-all duration-200 
-            hover:scale-110 border-[1px] border-zinc-400 h-auto aspect-square" onClick={scrollToTop}>
-                <Image src={Logo} className="w-6 h-6" alt="Logo"/>
+        <div className="flex flex-col fixed top-0 left-0 p-2">
+            <button className="btn btn-square w-14 h-14 transition-all duration-200 
+            hover:scale-110 ring-2 ring-zinc-500/50 " onClick={buttonPress}>
+                <Image src={Logo} className="w-6 h-6" alt="Logo" />
+
             </button>
         </div>
     )
