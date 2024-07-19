7b4c8c47bf2f08c289d2c2ad82ab9c9284f82e43
Jookare <134264039+Jookare@users.noreply.github.com>
2024-07-20 00:11:35 +0300

    Fix the noise

--- app/components/noise.js
+++ app/components/noise.js
@@ -1,51 +1,14 @@
-// const Noise = () => {
-//     return (
-
-//         <svg className="pointer-events-none fixed isolate z-40 opacity-70 mix-blend-soft-light">
-//             <filter id="noise">
-//                 <feTurbulence type="fractalNoise" baseFrequency="0.80" numOctaves="4" stitchTiles="stitch">
-//                 </feTurbulence>
-//             </filter>
-//             <rect  width="100%" height="100%" filter="url(#noise)"></rect>
-//         </svg>
-//     )
-// }
-// export default Noise;
-"use client"
-import { useEffect, useRef } from 'react';
-
 const Noise = () => {
-    // const svgRef = useRef(null);
 
-    // useEffect(() => {
-    //     const handleResize = () => {
-    //         if (svgRef.current) {
-    //             // svgRef.current.setAttribute('width', '100%');
-    //             svgRef.current.setAttribute('height', 'full');
-    //         }
-    //     };
-
-    //     // Attach resize event listener
-    //     window.addEventListener('resize', handleResize);
-
-    //     // Initial call to set the size
-    //     handleResize();
-
-    //     // Cleanup event listener on component unmount
-    //     return () => {
-    //         window.removeEventListener('resize', handleResize);
-    //     };
-    // }, []);
     return (
         <svg
-            // ref={svgRef} 
             className="pointer-events-none fixed isolate z-40 opacity-70 mix-blend-soft-light w-full h-full"
         >
             <filter id="noise">
                 <feTurbulence type="fractalNoise" baseFrequency="0.80" numOctaves="4" stitchTiles="stitch">
                 </feTurbulence>
             </filter>
-            <rect width="100%" height="100%" filter="url(#noise)"></rect>
+            <rect width="100vw" height="100vh" filter="url(#noise)"></rect>
         </svg>
     )
 }
